0f39c506f92b7a916c55db56a20bb3da628d707d
onlyalphy <onlyalphy01@gmail.com>
2024-03-08 17:59:28 +0300

    first commit

---
+++ user_data.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask user for their name
+name = input("What is your name?:Alphonce ")
+
+# Ask user for their age
+age = input("How old are you?:22 ")
+
+# Ask user for their location
+location = input("Where do you live?:Thika ")
+
+# Print personalized message
+print("Hello my name is" + (name) +"I am" +(age) + "years old and I live in" + (location))
